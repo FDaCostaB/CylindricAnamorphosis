7c07c058038f9c048b0852321f76362b8fac338a
YargonIV <fabien.da-costa-barros@etu.univ-grenoble-alpes.fr>
2020-03-26 15:16:51 +0100

    First file upload

---
+++ cmake-build-debug/CMakeFiles/CylindricAnamorphosis.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CylindricAnamorphosis.dir/main.c.obj"
+  "CylindricAnamorphosis.exe"
+  "CylindricAnamorphosis.exe.manifest"
+  "CylindricAnamorphosis.pdb"
+  "libCylindricAnamorphosis.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CylindricAnamorphosis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
