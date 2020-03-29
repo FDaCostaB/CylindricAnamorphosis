bf83b8bf20a7e8764ef8b792f3baa00a64d7b961
YargonIV <fabien.da-costa-barros@etu.univ-grenoble-alpes.fr>
2020-03-29 22:25:20 +0200

    Nouveau fichier executable de test et fichier de l'executable principale créé

--- test_pixelisation.c
+++ test_pixelisation.c
@@ -8,8 +8,35 @@
 #include "image.h"
 #include "geometrie3D.h"
 #include "types_erreur.h"
+#include "projection.h"
 
 int main(int argc, char * argv[]){
 
+    //void pixelisationPoint(TableauCoupleFlottant* Proj, Dictionnaire *res, int x, int y, Pixel val)
+
+    TableauCoupleFlottant *T;
+    Dictionnaire *res=nouveauDict();
+
+    Point2D A = { 0.0 , 0.0 }; Point2D B = { 0.0 , 0.0 };
+    Point2D D = { 0.0 , 0.0 }; Point2D E = { 0.0 , 0.0 };
+
+    T = creerTableauCoordonnees(1,1);
+
+
+    T->tab[0+0*T->L] = A;T->tab[1+0*T->L] = B;
+    T->tab[0+1*T->L] = C;T->tab[1+1*T->L] = D;
+
+    pixelisationPoint(T,res,0,0,55);
+    afficherDict(res);
+
+    /*        A = { 0.0 , 0.0  };         B = { 0.0 , 0.0 };         C = { };
+            D = { 0.0 , 0.0  }; Point2D E = { 0.0 , 0.0 }; Point2D F = { };
+    Point2D G = { -28.8, 10.4}; Point2D H = { }; Point2D I = { };
+
+    TableauCoupleFlottant *T = creerTableauCoordonnees(2,2);
+
+    T->tab[0+0*T->L] = A;T->tab[0+0*T->L] = A;T->tab[0+0*T->L] = A;
+    T->tab[0+0*T->L] = A;T->tab[0+0*T->L] = A;T->tab[0+0*T->L] = A;
+    T->tab[0+0*T->L] = A;T->tab[0+0*T->L] = A;T->tab[0+0*T->L] = A; */
 
 }
